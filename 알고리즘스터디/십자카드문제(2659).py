5c27b9c02fb4a2f7f6ff16ca07781be0c307fdba
WonSubSub <boxer1995@naver.com>
2023-01-03 19:11:05 +0900

    백준 십자카드문제(2659)

--- 알고리즘스터디/십자카드문제(2659).py
+++ 알고리즘스터디/십자카드문제(2659).py
@@ -1,30 +1,32 @@
-number = sorted(list(map(int, input().split(' '))))
-
-def sum_from_one(num):
-    sum = 0
-    for i in range(num+1):
-        sum += i
-    return sum
+number = list(map(int, input().split(' ')))
 
 ans = 0
 
-first = number[0]
-for i in range(1, first):
-    for a in range(1, 10 - i):
-        ans += sum_from_one(a)
+temp1 = [number[0], number[1], number[2], number[3]]
+temp2 = [number[1], number[2], number[3], number[0]]
+temp3 = [number[2], number[3], number[0], number[1]]
+temp4 = [number[3], number[0], number[1], number[2]]
+clock_number = min(temp1, temp2, temp3, temp4)
 
-second = number[1]
-for i in range(1, second):
-    ans += sum_from_one(i)
+def check_clock(number):
+    clock = 0
+    a = number // 1000
+    b = (number // 100) % 10
+    c = (number // 10) % 10
+    d = number % 10
+    numbers = [a, b, c, d]
+    temp1 = [numbers[0], numbers[1], numbers[2], numbers[3]]
+    temp2 = [numbers[1], numbers[2], numbers[3], numbers[0]]
+    temp3 = [numbers[2], numbers[3], numbers[0], numbers[1]]
+    temp4 = [numbers[3], numbers[0], numbers[1], numbers[2]]
+    clock_number = min(temp1, temp2, temp3, temp4)
+    if numbers == clock_number:
+        clock = 1
+    return clock
+        
 
-third = number[2]
-for i in range(1, third):
-    ans += 10 - i
-
-fourth = number[3]
-ans += fourth - 1
-
-if (first, second, third) == (1, 1, 1):
-    ans += 1
+for i in range(1111, clock_number[0]*1000 + clock_number[1]*100 + clock_number[2]*10 + clock_number[3] + 1):
+    if check_clock(i):
+        ans += 1
 
 print(ans)
