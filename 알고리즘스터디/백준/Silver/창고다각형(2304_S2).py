dd29312816ae4d5501450a1027b9e9b0beea06cf
WonSubSub <boxer1995@naver.com>
2023-01-06 14:23:44 +0900

    백준 창고다각형(2304_S2)

--- 알고리즘스터디/백준/Silver/창고다각형(2304_S2).py
+++ 알고리즘스터디/백준/Silver/창고다각형(2304_S2).py
@@ -9,10 +9,37 @@ for i in range(n):
 
 ch.sort()
 for i in range(n):
-    cordinates.append(ch[0])
-    heights.append(ch[1])
+    cordinates.append(ch[i][0])
+    heights.append(ch[i][1])
 
-print(cordinates)
-print(heights)
+left_max_index = heights.index(max(heights))
+heights_reverse = heights[::-1]
+cordinates_reverse = cordinates[::-1]
+right_max_index = n - 1 - heights_reverse.index(max(heights_reverse))
+
+if heights.count(heights[left_max_index]) > 1:
+    for i in range(left_max_index, (right_max_index + 1)):
+        heights[i] = max(heights)
+
+for i in range(1, left_max_index):
+    if heights[i] < heights[i-1]:
+        heights[i] = heights[i-1]
+
+max_index_reverse = heights_reverse.index(max(heights_reverse))
+for i in range(1, max_index_reverse):
+    if heights_reverse[i] < heights_reverse[i-1]:
+        heights_reverse[i] = heights_reverse[i-1]
+
+ans = 0
+
+ans += max(heights) * (cordinates[right_max_index] - cordinates[left_max_index] + 1)
+
+for i in range(left_max_index):
+    ans += (cordinates[i+1] - cordinates[i]) * heights[i]
+
+for i in range(max_index_reverse):
+    ans -= (cordinates_reverse[i+1] - cordinates_reverse[i]) * heights_reverse[i]
+
+print(ans)
 
 
