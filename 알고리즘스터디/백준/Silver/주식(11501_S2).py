9d8d4146d87e3c661f1592ea9c792ff6679e27de
WonSubSub <boxer1995@naver.com>
2023-01-08 13:45:59 +0900

    .

--- 알고리즘스터디/백준/Silver/주식(11501_S2).py
+++ 알고리즘스터디/백준/Silver/주식(11501_S2).py
@@ -7,7 +7,7 @@ for i in range(n):
     answer = 0
     days = int(input())
     prices = list(map(int, input().split()))
-    
+
     max_price = prices[-1]
     for i in range(days-2, -1, -1):
         if prices[i] <= max_price:
