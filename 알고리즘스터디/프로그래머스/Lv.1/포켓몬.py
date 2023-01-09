61817a240fa5a130071940b37a0a57c1798b5819
WonSubSub <boxer1995@naver.com>
2023-01-09 17:15:43 +0900

    포켓몬

---
+++ 알고리즘스터디/프로그래머스/Lv.1/포켓몬.py
@@ -0,0 +1,8 @@
+def solution(nums):
+    answer = 0
+    if int(len(nums)/2) < len(set(nums)):
+        answer = int(len(nums)/2)
+    else:
+        answer = len(set(nums))
+                     
+    return answer
