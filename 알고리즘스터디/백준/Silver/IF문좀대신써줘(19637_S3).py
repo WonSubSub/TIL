1ab85175fe22851c9b730d94a7cdabc328aa881e
WonSubSub <boxer1995@naver.com>
2023-01-10 22:13:28 +0900

    IF문좀대신써줘(시간과의 싸움에서 승리)

--- 알고리즘스터디/백준/Silver/IF문좀대신써줘(19637_S3).py
+++ 알고리즘스터디/백준/Silver/IF문좀대신써줘(19637_S3).py
@@ -3,34 +3,82 @@ input = sys.stdin.readline
 
 title, player = map(int, input().split())
 
-title_name = []
+title_name = []           # 리스트 두개
 title_number = [0]
 for i in range(title):
     name, number = input().split()
-    if int(number) not in title_number:
+    if title_number[-1] != int(number) + 1:
         title_name.append(name)
-        title_number.append(int(number))
+        title_number.append(int(number)+1)
 
-def search(player_number, title_number):
-    if len(title_number) == 1:
-        if player_number == title_number[0]:
-            return title_number[0], 1
-        else:
-            return title_number[0], 0
+# titles = [[0,0]]            # 이중리스트
+# for i in range(title):
+#     name, number = input().split()
+#     # if titles[-1][1] != int(number) + 1:
+#     titles.append([name, int(number) + 1])
+
+# titles = {0: 'a'}           # 딕셔너리
+# for i in range(title):
+#     name, number = input().split()
+#     if (int(number) + 1) not in titles.keys():
+#         titles[int(number)+1] = name
+# title_number = list(titles.keys())
+
+def search(score, title_number, s, e):
+    m = int((s+e)/2)
+    if score == title_number[m]:       # 리스트 두개 재귀
+        answer = title_name[m]
+        return answer
+    elif e-s == 1:
+        return title_name[s]
+    elif score < title_number[m]:
+        return search(score, title_number, s, m)
     else:
-        if player_number < title_number[int(len(title_number)/2)]:
-            return search(player_number, title_number[:int(len(title_number)/2)])
-        else:
-            return search(player_number, title_number[int(len(title_number)/2):])
+        return search(score, title_number, m, e)
+    
+    # answer = 0               # 리스트 두개 반복문
+    # m = int((s+e)/2)
+    # while True:
+    #     if title_number[m] <= score < title_number[m+1] :
+    #         answer = title_name[m]
+    #         break
+    #     elif score < title_number[m]:
+    #         e = m
+    #     else:
+    #         s = m
+    #     m = int((s+e)/2)
+    # return answer
+
+    # answer = 0              # 이중리스트 반복문
+    # m = int((s+e)/2)
+    # while True:
+    #     if titles[m][1] <= score < titles[m+1][1] :
+    #         answer = titles[m+1][0]
+    #         break
+    #     elif score < titles[m][1]:
+    #         e = m
+    #     else:
+    #         s = m
+    #     m = int((s+e)/2)
+    # return answer
+
+    # answer = 0            # 딕셔너리
+    # while True:
+    #     if title_number[int((s+e)/2)] <= score < title_number[int((s+e)/2)+1] :
+    #         answer = titles[title_number[int((s+e)/2)+1]]
+    #         break
+    #     elif score < title_number[int((s+e)/2)]:
+    #         e = (s+e)/2
+    #     else:
+    #         s = (s+e)/2
+    # return answer
+
+# for i in range(player):               # 이중리스트
+#     score = int(input())
+#     print(search(score, titles, 0, len(titles)-1))
 
 for i in range(player):
-    player_number = int(input())
-    player_title, b = search(player_number, title_number)
-    if player_number == 0:
-        print(title_name[0])
-    else:
-        if b:
-            print(title_name[title_number.index(player_title)-1])
-        else:
-            print(title_name[title_number.index(player_title)])
+    score = int(input())
+    print(search(score, title_number, 0, len(title_number)-1))
+
     
---
+++ 알고리즘스터디/백준/Silver/가장긴감소하는부분수열(11722_S2).py
@@ -0,0 +1,19 @@
+n = int(input())
+
+numbers = list(map(int, input().split()))
+
+answer = 0
+
+max_index = numbers.index(max(numbers[::-1]))
+new_numbers = numbers[:max_index+1]
+numbers = numbers[max_index+1:]
+
+min_num = min(new_numbers)
+# for i in range(len(new_numbers)):
+
+
+
+soo_list = []
+def soo(soo_list, numbers, i):
+    
+# [10,11,12,13,14,15,16,5,2,4,1,2,5,3,2,5,4,3,2,3,1,4,2,1]
