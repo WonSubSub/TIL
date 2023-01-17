9ac62462edc12c71c8c517527dffbf697afdf65e
WonSubSub <boxer1995@naver.com>
2023-01-17 15:38:42 +0900

    RGB

--- 알고리즘스터디/백준/Silver/RGB(1149_S1).py
+++ 알고리즘스터디/백준/Silver/RGB(1149_S1).py
@@ -1,37 +1,20 @@
 n = int(input())
 
 matrix = []
+red = 0
+green = 0
+blue = 0
 for i in range(n):
     numbers = list(map(int, input().split()))
-    matrix.append(numbers)
-
-[[9,1,11],
- [280,280,291],
- [100,1,100],
- [200,1,200],
- [800,1,800],
- [650,1,650]]
-[26, 49, 13]
-[40, 60, 89]
-[83, 57, 99]
-
-answer = 0
-for i in range(len(matrix)):
     if i == 0:
+        red += numbers[0]
+        green += numbers[1]
+        blue += numbers[2]
+        matrix.append([red, green, blue])
+    else:
+        red = numbers[0] + min(matrix[i-1][1], matrix[i-1][2])
+        green = numbers[1] + min(matrix[i-1][0], matrix[i-1][2])
+        blue = numbers[2] + min(matrix[i-1][1], matrix[i-1][0])
+        matrix.append([red, green, blue])
 
-71
-39
-44
-
-32 39
-83 44
-55 39
-
-51 55 39
-37 32 39
-63 32 39
-
-89 37 32 39
-29 63 32 39
-100 37 32 39
-dp
+print(min(matrix[-1][0], matrix[-1][1], matrix[-1][2]))
