52c1b1d89e9fcec37926fa7971eb49f3909b0f18
WonSubSub <boxer1995@naver.com>
2023-01-21 21:22:32 +0900

    게임 맵 최단거리

--- 알고리즘스터디/프로그래머스/Lv.2/게임맵최단거리.py
+++ 알고리즘스터디/프로그래머스/Lv.2/게임맵최단거리.py
@@ -1,6 +1,33 @@
-visit = [[0,0]]             # dfs --> 효율성 X
-ans_list = []
-cnt = 1
+from collections import deque           # bfs풀이
+import sys
+sys.setrecursionlimit(10**6)
+moves = [[1,0], [0,1], [-1,0], [0,-1]]
+answer = 0
+
+def solution(maps):
+    
+    q = deque([[0,0,1]])
+    def bfs(maps, q):
+        global moves, answer
+        n = len(maps)
+        m = len(maps[0])
+        current = q.popleft()
+        x, y, answer = current[0], current[1], current[2]
+        for a,b in moves:
+            if [x+a,y+b] == [n-1,m-1]:
+                return answer+1
+            if 0<=x+a<=n-1 and 0<=y+b<=m-1 and maps[x+a][y+b] == 1:
+                maps[x+a][y+b] = 0
+                q.append([x+a,y+b,answer+1])
+        if len(q) == 0:
+            return -1
+        return bfs(maps, q)
+
+    return bfs(maps, q)
+
+
+
+ans_list = []               # dfs풀이 --> 효율성 X
 move_list = [[1,0], [0,1], [-1,0], [0,-1]]
 
 def solution(maps):
@@ -8,28 +35,28 @@ def solution(maps):
     n = len(maps)
     m = len(maps[0])
     start = [0,0]
-    def dfs(maps,start):
-        global visit, ans_list, cnt, move_list
+    cnt = 1
+    def dfs(maps,start,cnt):
+        global ans_list, move_list
         x = start[0]
         y = start[1]
         for a,b in move_list:
-            if 0<=x+a<=n-1 and 0<=y+b<=m-1 and maps[x+a][y+b] == 1 and [x+a,y+b] not in visit:
+            if 0<=x+a<=n-1 and 0<=y+b<=m-1 and maps[x+a][y+b] == 1 and maps[x+a][y+b] == 1:
                 if [x+a,y+b] == [n-1,m-1]:
-                    cnt += 1
-                    visit.append([x+a,y+b])
-                    ans_list.append(cnt)
-                    cnt -= 1
+                    maps[x+a][y+b] = 0
+                    ans_list.append(cnt+1)
+                    maps[x+a][y+b] = 1
                     break
                 else:
-                    cnt += 1
-                    visit.append([x+a,y+b])
-                    dfs(maps, [x+a,y+b])
-                    visit.pop()
-                    cnt -= 1
+                    if len(ans_list) > 0 and cnt > max(ans_list):
+                        break
+                    maps[x+a][y+b] = 0
+                    dfs(maps, [x+a,y+b], cnt+1)
+                    maps[x+a][y+b] = 1
                     
         return ans_list
     
-    ans_list = dfs(maps,start)   
+    ans_list = dfs(maps,start,cnt)   
     if ans_list == []:
         answer = -1
     else:
