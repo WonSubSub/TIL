578f8258eb1927374265e4fcde7ace8c07bf2d25
WonSubSub <boxer1995@naver.com>
2023-01-24 15:09:04 +0900

    배달 bfs풀이

--- 알고리즘스터디/프로그래머스/Lv.2/배달.py
+++ 알고리즘스터디/프로그래머스/Lv.2/배달.py
@@ -1,4 +1,30 @@
-def solution(N, road, K):
+from collections import deque
+def solution(N, road, K):           # bfs풀이 마을별 걸리는 최소 시간 리스트 추가
+    q = deque([[1,K]])
+    ans_set = set([1])
+    cost = [0] * (N+1)
+
+    while q:
+        current, K = q.popleft()
+        temp = len(q)
+        for i in range(len(road)):
+            if road[i][0] == current:
+                if road[i][2] <= K:
+                    if K-road[i][2] >= cost[road[i][1]]:
+                        cost[road[i][1]] = K-road[i][2]
+                        ans_set.add(road[i][1])
+                        q.append([road[i][1], K-road[i][2]])
+            if road[i][1] == current:
+                if road[i][2] <= K:
+                    if K-road[i][2] >= cost[road[i][0]]:
+                        cost[road[i][0]] = K-road[i][2]
+                        ans_set.add(road[i][0])
+                        q.append([road[i][0], K-road[i][2]])
+    answer = len(ans_set)
+    
+    return answer
+
+def solution(N, road, K):           # dfs풀이 --> 32번테케 시간초과
     ans_set = set([1])
     current = 1
     visit = [0] * (N+1)
