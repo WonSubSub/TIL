99294399d52206c0b22d6c30a7f33c43491fec62
WonSubSub <boxer1995@naver.com>
2023-01-25 18:48:10 +0900

    더 맵게 다른  추가

--- 알고리즘스터디/프로그래머스/Lv.2/더맵게.py
+++ 알고리즘스터디/프로그래머스/Lv.2/더맵게.py
@@ -10,3 +10,17 @@ def solution(scoville, K):
         count+=1
 
     return count
+
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    
+    while scoville[0] < K and len(scoville) > 1:
+        answer += 1
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+        heapq.heappush(scoville, first + second*2)
+    if scoville[0] < K:
+        answer = -1
+    return answer
