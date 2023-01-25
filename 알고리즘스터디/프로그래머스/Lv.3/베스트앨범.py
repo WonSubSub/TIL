f4b6adf35650cb53e23b5e8ff4c3dbf82b70da48
WonSubSub <boxer1995@naver.com>
2023-01-25 16:55:48 +0900

    베스트앨범

--- 알고리즘스터디/프로그래머스/Lv.3/베스트앨범.py
+++ 알고리즘스터디/프로그래머스/Lv.3/베스트앨범.py
@@ -1,26 +1,37 @@
+from collections import deque
+
 def solution(genres, plays):
     answer = []
     
     genre_dict = {}
-    genre_sum = []
-    for genre in set(genres):
-        genre_dict[genre] = []
-    
+    genre_play_dict = {}
     for i in range(len(genres)):
-        genre_dict[genres[i]].append(plays[i])
-        
-    for genre in genre_dict.keys():
-        genre_sum.append([sum(genre_dict[genre]), genre])
+        if genres[i] in genre_dict.keys():
+            genre_dict[genres[i]] += plays[i]
+        else:
+            genre_dict[genres[i]] = plays[i]
+        if genres[i] in genre_play_dict.keys():
+            genre_play_dict[genres[i]].append([plays[i], i])
+        else:
+            genre_play_dict[genres[i]] = [[plays[i], i]]
     
-    genre_sum.sort(reverse=True)
+    for key in genre_play_dict.keys():
+        genre_play_dict[key].sort(key= lambda x: (-x[0], x[1]))
     
-    for i in range(len(genre_sum)):
-        g = genre_sum[i][1]
+    play_list = sorted(list(genre_dict.values()))[::-1]
+    popular = []
+    for play in play_list:
+        for key in genre_dict.keys():
+            if genre_dict[key] == play:
+                popular.append(key)
+                
+    for i in range(len(popular)):
+        q = deque(genre_play_dict[popular[i]])
         cnt = 0
-        for a in range(len(genre_dict[g])):
-            genre_dict[g].sort(reverse=True)
-            answer.append(plays.index(genre_dict[g][a]))
+        while q:
             cnt += 1
+            answer.append(q.popleft()[1])
             if cnt == 2:
                 break
+                
     return answer
